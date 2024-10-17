9403cb713fd9017392e1610a02ef9a8ca78745d6
Wesley Klaassen <140204064+wnkinc@users.noreply.github.com>
2024-10-16 18:37:22 -0700

    Initial commit

---
+++ src/App.test.jsx
@@ -0,0 +1,36 @@
+// App.test.jsx
+
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button
+          type="button"
+        >
+          Click Me
+        </button>
+        <h1>
+          Magnificent Monkeys
+        </h1>
+      </div>
+    `);
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
